1a889715185b9b8d5ea86feeb3ad2c887a766561
gpedro178 <gpedro178@gmail.com>
2021-07-15 11:15:16 -0300

    Concatenador de TXT

---
+++ concatenador.py
@@ -0,0 +1,20 @@
+# This script will merge .txt files that find in the same folder
+# into one file.
+
+import os
+
+# Reading all the files in the folder
+all_files = os.listdir()
+
+# Turning the previous reading into a list and filtering only .txt
+txt_files = list(filter(lambda x: x[-4:] == ".txt", all_files))
+
+# print(txt_files)
+
+# Creating a file in write mode and using a loop to fill it, 
+# the output file name can be changed in line 16
+with open("fusion-liq-elec.txt","w") as resultado:
+    for archivos in txt_files: # Looping the txt_files
+        with open(archivos) as contenido:  # Opening the txt_files
+            resultado.write(contenido.read()) # Writing what is read on the final file
+        resultado.write("\n") # Insert the next content in a new line
